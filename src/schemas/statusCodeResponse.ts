0849851045bf2a5c723f39ed8e3d987759bd74c9
Mikey Haklander <mikey.haklander@capgemini.com>
2023-12-27 14:44:04 +0100

    zod validation

---
+++ src/schemas/statusCodeResponse.ts
@@ -0,0 +1,17 @@
+import { isObject } from "@/types/guards/isObject";
+import { z } from "zod";
+
+export const statusCodeResponse = z.object({
+  message: z.string(),
+  test: z.string(),
+});
+
+export function isStatusCodeReponse(obj: unknown): obj is StatusCodeResponse {
+  if (!isObject(obj)) return false;
+
+  const hasMessage = "message" in obj && typeof obj.message === "string";
+
+  return hasMessage;
+}
+
+export type StatusCodeResponse = z.infer<typeof statusCodeResponse>;
--- src/services/service.ts
+++ src/services/service.ts
@@ -1,20 +1,29 @@
 import HTTP from "@/lib/axios";
 import { handleServiceErrors } from "@/lib/handleServiceErrors";
+import {
+  StatusCodeResponse,
+  isStatusCodeReponse,
+  statusCodeResponse,
+} from "@/schemas/statusCodeResponse";
 import { ServiceCallResult } from "@/types/serviceCall";
-
-type SuccessTestResponse = {
-  message: string;
-};
 
 /** Would be a service method */
 export async function callService(
   code: string,
   signal: AbortSignal
-): Promise<ServiceCallResult<SuccessTestResponse>> {
+): Promise<ServiceCallResult<StatusCodeResponse>> {
   try {
-    const { data } = await HTTP.get<SuccessTestResponse>(`/api/${code}`, {
+    const { data } = await HTTP.get<StatusCodeResponse>(`/api/${code}`, {
       signal,
     });
+
+    /** Zod validation */
+    statusCodeResponse.parse(data);
+
+    // /** Custom typeguard */
+    // if (!isStatusCodeReponse(data)) {
+    //   throw new Error("Data not of type statusCodeResponse");
+    // }
 
     return {
       canceled: false,
